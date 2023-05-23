3ce70f9fed5b9d3258f8feb5f57d72fc16f922b4
Louise Findlay <louise.findlay@prismic.io>
2023-05-23 12:48:01 +0100

    Added Google Fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,26 @@
 import { Layout } from "components";
 import { SiteProvider } from "context/SiteContext";
+import { Inter, Arvo } from "next/font/google";
 import "@/styles/globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+const arvo = Arvo({
+  weight: ["700"],
+  subsets: ["latin"],
+});
 
 export default function App({ Component, pageProps }) {
   return (
     <SiteProvider>
       <Layout>
+        <style jsx global>
+          {`
+            :root {
+              --heading-font: ${arvo.style.fontFamily};
+              --body-font: ${inter.style.fontFamily};
+            }
+          `}
+        </style>
         <Component {...pageProps} />
       </Layout>
     </SiteProvider>
