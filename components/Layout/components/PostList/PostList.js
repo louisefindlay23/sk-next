37efa4ed9a9bb4612913013536defd5e92ee4341
Louise Findlay <louise.findlay@prismic.io>
2023-05-23 13:25:01 +0100

    Working getPreviousPosts function

--- components/Layout/components/PostList/PostList.js
+++ components/Layout/components/PostList/PostList.js
@@ -1,13 +1,31 @@
 // TODO: Query all posts at once and then selectively return for Previous and Next buttons. Also, /page/1 etc. or ?page=1.
+import { useState, useCallback } from "react";
 import { PrismicLink, PrismicRichText } from "@prismicio/react";
 import { PrismicNextImage } from "@prismicio/next";
 import Date from "components/Layout/components/Date/Date";
 import styles from "./PostList.module.css";
 
 function PostList({ posts }) {
+  const [showPosts, setPosts] = useState(posts.slice(2, 3));
+  const [postPage, setPostPage] = useState(3);
+
+  const getPreviousPosts = useCallback(() => {
+    const previousPosts = posts.slice(postPage - 2, postPage - 1);
+    const previousPage = postPage - 1;
+    setPosts(previousPosts);
+    setPostPage(previousPage);
+  }, [posts, postPage]);
+
+  const getNextPosts = useCallback(() => {
+    const nextPosts = posts.slice(postPage + 2, postPage + 1);
+    const nextPage = postPage + 1;
+    setPosts(nextPosts);
+    setPostPage(nextPage);
+  }, [posts, postPage]);
+
   return (
     <>
-      {posts.map((post) => (
+      {showPosts.map((post) => (
         <article key={JSON.stringify(post.data)}>
           <div className={styles.boxContent}>
             <PrismicLink document={post}>
@@ -24,6 +42,10 @@ function PostList({ posts }) {
           </div>
         </article>
       ))}
+      <div className={styles.pagination}>
+        <button onClick={getPreviousPosts}>Previous</button>
+        <button onClick={getNextPosts}>Next</button>
+      </div>
     </>
   );
 }
